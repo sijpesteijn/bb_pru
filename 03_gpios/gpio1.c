9c750ee68f528722a7e08b722bb2258a14f551e8
gijs.sijpesteijn <gijs@jpoint.nl>
2019-04-14 17:29:45 +0200

    initial

--- 03_gpios/gpio1.c
+++ 03_gpios/gpio1.c
@@ -6,16 +6,23 @@
 #define GPIO0	0x44e07000		// GPIO Bank 0  See Table 2.2 of TRM <1>
 #define GPIO_SETDATAOUT		0x194	// For setting the GPIO registers
 #define GPIO_CLEARDATAOUT	0x190	// For clearing the GPIO registers
+
 #define P8_13	(0x1<<23)			// Bit position tied to P8_13
 #define P8_14	(0x1<<26)			// Bit position tied to P8_14
 #define P8_17	(0x1<<27)			// Bit position tied to P8_17
 #define P8_19	(0x1<<22)			// Bit position tied to P8_19
-//#define P8_31	(0x1<<10)			// Bit position tied to P8_31
 
 #define P9_11	(0x1<<30)			// Bit position tied to P9_11
-//#define P9_13	(0x1<<31)			// Bit position tied to P9_13
+#define P9_13	(0x1<<31)			// Bit position tied to P9_13
 #define P9_17	(0x1<<5)			// Bit position tied to P9_17
-//#define P9_26	(0x1<<14)			// Bit position tied to P9_26
+#define P9_18	(0x1<<4)			// Bit position tied to P9_18
+#define P9_19	(0x1<<13)			// Bit position tied to P9_19
+#define P9_20	(0x1<<12)			// Bit position tied to P9_20
+#define P9_21	(0x1<<3)			// Bit position tied to P9_21
+#define P9_22	(0x1<<2)			// Bit position tied to P9_22
+#define P9_24	(0x1<<15)			// Bit position tied to P9_24
+#define P9_26	(0x1<<14)			// Bit position tied to P9_26
+
 #define deley   10000000
 
 void main(void)
