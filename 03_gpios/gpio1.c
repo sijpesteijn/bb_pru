441d173ae79213ede2b8aaa9730136f2d23e5817
gijs.sijpesteijn <gijs@jpoint.nl>
2019-04-14 19:42:35 +0200

    initial

--- 03_gpios/gpio1.c
+++ 03_gpios/gpio1.c
@@ -122,8 +122,8 @@ void main(void)
 	uint32_t *gpio0 = (uint32_t *)GPIO0;
 	uint32_t gpios[] = { P8_13, P8_14, P8_17, P8_19, P9_11, P9_13, P9_17, P9_18, P9_19, P9_20, P9_21, P9_22, P9_24, P9_26 };
 	while(1) {
-		wiggle(gpios);
-//		leftToRight(gpios);
+//		wiggle(gpios);
+		leftToRight(gpios);
 	}
 }
 
