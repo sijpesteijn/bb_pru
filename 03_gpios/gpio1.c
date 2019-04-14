a93a9323c2a9f90c55713e14e96d83e07a069141
gijs.sijpesteijn <gijs@jpoint.nl>
2019-04-14 20:08:35 +0200

    initial

--- 03_gpios/gpio1.c
+++ 03_gpios/gpio1.c
@@ -3,7 +3,6 @@
 #include <pru_cfg.h>
 #include "resource_table_empty.h"
 
-volatile register uint32_t __R30;
 #define GPIO0	0x44e07000			// GPIO Bank 0  See Table 2.2 of TRM <1>
 #define GPIO_SETDATAOUT		0x194	// For setting the GPIO registers
 #define GPIO_CLEARDATAOUT	0x190	// For clearing the GPIO registers
@@ -27,7 +26,7 @@ volatile register uint32_t __R30;
 #define deley   10000000
 
 void leftToRight(uint32_t gpios[]) {
-	uint32_t *gpio0 = (uint32_t *)__R30;
+	uint32_t *gpio0 = (uint32_t *)GPIO0;
 	gpio0[GPIO_SETDATAOUT/4] = P8_13;
 	__delay_cycles(deley);
 	gpio0[GPIO_SETDATAOUT/4] = P8_14;
