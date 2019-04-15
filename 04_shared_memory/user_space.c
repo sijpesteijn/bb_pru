49f4e8b24af1656084b0e593c40d05f2a4475a0a
gijs.sijpesteijn <gijs@jpoint.nl>
2019-04-15 22:09:47 +0200

    initial

--- 04_shared_memory/user_space.c
+++ 04_shared_memory/user_space.c
@@ -11,9 +11,15 @@ int main (void) {
     unsigned int ret;
     tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
 
-    printf("\nINFO: Starting %s example.\r\n", "dmx");
+    printf("\nINFO: Starting %s example.\r\n", "pru_memory");
     /* Initialize the PRU */
     prussdrv_init();
 
     prussdrv_pruintc_init(&pruss_intc_initdata);
+
+    prussdrv_exec_program (PRU_NUM0, "./example.bin");
+
+    prussdrv_pru_wait_event (PRU_EVTOUT_0);
+
+    prussdrv_pru_clear_event (PRU_EVTOUT0, PRU0_ARM_INTERRUPT);
 }
